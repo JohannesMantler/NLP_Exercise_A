4888ce79db37287164dc01f51db1ed5cdf95ec77
Johannes Mantler <if23b174@technikum-wien.at>
2025-10-16 11:01:46 +0200

    Exercise A1

---
+++ Exercise_A.py
@@ -0,0 +1,21 @@
+import re
+import string
+
+file = "Taylor_Swift_Quotes.txt"
+
+def clean(text):
+    text = text.lower()                                                 #Lowercase
+    text = re.sub(r'\d+', " ", text)                        #Remove digits
+    text = text.translate(str.maketrans('', '', string.punctuation))    #Remove punctuation
+    text = re.sub(r'\s+', ' ', text).strip()                #Remove überflüssige Leerzeichen
+    return text
+
+with open(file) as file:
+    text = file.read()
+
+cleaned_text = clean(text)
+
+print(cleaned_text)
+
+with open('cleaned_text.txt', 'w') as file:
+    file.write(cleaned_text)
